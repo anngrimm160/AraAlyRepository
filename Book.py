456bb767b2fcc8bb81478c073f42c2f1f9e1ff08
Anandhi Narayan <anandhi910@gmail.com>
2023-10-05 04:23:52 -0400

    Create Book.py

---
+++ Book.py
@@ -0,0 +1,13 @@
+
+userName = str(input("What is your name?: "))
+userAge = int(input("How old are you?: "))
+
+print("Your name is", userName, "and you are", userAge, "years old.")
+
+print()
+print()
+print()
+
+bookName = str(input("What's you're favorite book?: "))
+authorName = str(input("Who wrote this book?: "))
+
