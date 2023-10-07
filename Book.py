a876f961fc116b4d4c77e8338022b59a52c1bf16
ALY48CS <134087807+ALY48CS@users.noreply.github.com>
2023-10-07 08:49:41 -0400

    Update Book.py

--- Book.py
+++ Book.py
@@ -8,6 +8,8 @@ print()
 print()
 print()
 
-bookName = str(input("What's you're favorite book?: "))
+bookName = str(input("What's your favorite book?: "))
 authorName = str(input("Who wrote this book?: "))
 
+combined_list = [userName, userAge, bookName, authorName]
+print("Thank you for answering " + combined_list[0] + ", it is interesting to know that " + str(combined_list[1]) + " year olds are reading " + combined_list[2] + " by " + combined_list[3] + ".")
