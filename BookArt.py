535498ca93da167ea4ebcac34ba33d7fb2d8cde4
Anandhi Narayan <anandhi910@gmail.com>
2023-10-13 16:16:15 -0400

    Update and rename Book.py to BookArt.py

--- BookArt.py
+++ BookArt.py
@@ -13,3 +13,19 @@ authorName = str(input("Who wrote this book?: "))
 
 combined_list = [userName, userAge, bookName, authorName]
 print("Thank you for answering " + combined_list[0] + ", it is interesting to know that " + str(combined_list[1]) + " year olds are reading " + combined_list[2] + " by " + combined_list[3] + ".")
+
+print()
+print()
+print()
+
+art = str(input("Do you have a favorite artist? (Y/N): "))
+
+art= art.upper()
+
+if (art == "Y"):
+  artist = str(input("Who is it?: "))
+  print()
+  print("I see,", userName + "! You like the author", authorName, "and the artist", artist)
+
+else:
+  print("That's okay, perhaps you should visit the museum for inspiration!")
